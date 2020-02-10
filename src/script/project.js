c1244224de735a02878e28136f1f12ebd7cfdbe1
dev1980 <brham1980@gmail.com>
2020-02-10 21:49:31 +0545

    method for add todo item and show list

--- src/script/project.js
+++ src/script/project.js
@@ -1,9 +1,19 @@
 export class Project {
   constructor(name) {
     this._name = name;
+    this.todoList = [];
   }
 
   get name() {
     return this._name;
   }
+  
+  addToDo(item){
+    this.todoList.push(item);
+  }
+showList(){
+    return this.todoList;
+}
+
+
 }
