f4d00f6cc363cdd6d29ee76587f86b4145db7fb3
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-10 07:12:35 -0600

    add getProjectName and addProjectForm functions

--- src/index.js
+++ src/index.js
@@ -3,16 +3,30 @@ import './script/todo';
 import './script/project';
 
 const addProject = document.getElementById('add-project');
+const projectName = document.getElementById('projectName');
+const projectList = document.getElementById('projectList');
+const submitProject = document.getElementById('submit');
 
 function addProjectForm() {
+  console.log('add project clicked');
   const projectForm = document.getElementById('projectForm');
-  if (projectForm.style.display === 'none') {
+  const projectBtn = projectForm.style.display;
+  if (projectBtn == 'none') {
     projectForm.style.display = 'block';
   } else {
     projectForm.style.display = 'none';
   }
 }
 
+function getProjectName() {
+  if (projectName.value != '') {
+    const project = document.createElement('div');
+    project.textContent = projectName.value;
+    projectList.appendChild(project);
+  }
+}
+
 addProject.addEventListener('click', addProjectForm);
+submitProject.addEventListener('click', getProjectName);
 
 console.log('Message for webpack');
