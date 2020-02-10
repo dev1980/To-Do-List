fbfe3399e14712ee90c922c1f6ac755ca16cc02c
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-10 06:37:23 -0600

    add function to display the project form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import "./style/style.css";
-import "./script/todo";
-import "./script/project";
+import './style/style.css';
+import './script/todo';
+import './script/project';
 
-console.log("Message for webpack");
+const addProject = document.getElementById('add-project');
+
+function addProjectForm() {
+  const projectForm = document.getElementById('projectForm');
+  if (projectForm.style.display === 'none') {
+    projectForm.style.display = 'block';
+  } else {
+    projectForm.style.display = 'none';
+  }
+}
+
+addProject.addEventListener('click', addProjectForm);
+
+console.log('Message for webpack');
